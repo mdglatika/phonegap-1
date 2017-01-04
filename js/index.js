a5e55b4e0ef1fdce539e912cd81278ac0227c15f
mdglatika <mdglatika@gmail.com>
2017-01-03 23:18:15 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,6 +27,29 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        
+
+        function onDeviceReady() {
+          window.plugins.sim.getSimInfo(successCallback, errorCallback);
+        }
+
+        function successCallback(result) {
+          alert(result);
+        }
+
+        function errorCallback(error) {
+          alert(error);
+        }
+
+        // Android only: check permission
+        function hasReadPermission() {
+          window.plugins.sim.hasReadPermission(successCallback, errorCallback);
+        }
+
+        // Android only: request permission
+        function requestReadPermission() {
+          window.plugins.sim.requestReadPermission(successCallback, errorCallback);
+        }
     },
     // deviceready Event Handler
     //
